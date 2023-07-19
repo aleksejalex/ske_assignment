f555c6bf13e18e69ff490a49517f7250626fcb72
aleksejalex <29196199+aleksejalex@users.noreply.github.com>
2023-07-19 12:09:22 +0200

    Create testfile.py

---
+++ testfile.py
@@ -0,0 +1,12 @@
+import inspect
+import numpy as np
+import scipy as scp
+import pandas as pd
+import seaborn as sbn
+import csv
+import matplotlib as mpt
+import statsmodels
+import statsmodels.formula.api as sm
+from matplotlib import pyplot as plt
+import reliability as rel
+import lifelines
